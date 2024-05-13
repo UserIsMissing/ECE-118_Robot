b7f53623de6b7cc4d79431cdae32eb670351ef99
QingCao25 <qcao10@ucsc.edu>
2024-05-13 11:47:32 -0700

    Renamed Folder

---
+++ Code/Testing-Code.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Testing-Code.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Testing-Code.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=testing-code.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/testing-code.x.tar
